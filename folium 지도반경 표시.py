123c5e6243e8725c754d7c59990761f4110e3c69
cassil <mintaesshi@nate.com>
2019-09-18 18:58:06 +0900

    folium 지도반경 표시 py
    folium 지도반경 표시

---
+++ folium 지도반경 표시.py
@@ -0,0 +1,1034 @@
+import folium
+map2 = folium.Map(location=[37.560179, 126.995988], zoom_start=12)
+
+# folium.CircleMarker([37.528700, 127.116811], radius=5, popup='mola', color='#3186cc', fill_color='#3186cc').add_to(map2)
+
+folium.CircleMarker(
+    location=[37.528700, 127.116811],
+    radius=70,
+    popup='풍납파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.494362, 126.988821],
+    radius=70,
+    popup='이수파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.565016, 126.967017],
+    radius=70,
+    popup='서울서대문경찰서',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.576032, 126.924023],
+    radius=70,
+    popup='남가좌파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.582970, 126.912862],
+    radius=70,
+    popup='북가좌파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.558516, 126.942953],
+    radius=70,
+    popup='신촌지구대',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.570241, 126.932839],
+    radius=70,
+    popup='연희파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.569707, 126.961125],
+    radius=70,
+    popup='충정로지구대',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.583555, 126.936191],
+    radius=70,
+    popup='홍은2파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.595209, 126.946395],
+    radius=70,
+    popup='홍은파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.588167, 126.944508],
+    radius=70,
+    popup='홍제파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.602170, 126.921279],
+    radius=70,
+    popup='서울서부경찰서',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.608744, 126.931826],
+    radius=70,
+    popup='녹번파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.591828, 126.905357],
+    radius=70,
+    popup='신사지구대',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.590496, 126.917522],
+    radius=70,
+    popup='응암3파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.601597, 126.926434],
+    radius=70,
+    popup='응암지구대',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.495595, 127.005264],
+    radius=70,
+    popup='서울서초경찰서',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.461919, 127.051189],
+    radius=70,
+    popup='내곡파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.505971, 127.005226],
+    radius=70,
+    popup='반포지구대',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.492293, 127.028150],
+    radius=70,
+    popup='서초2파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.491930, 127.010796],
+    radius=70,
+    popup='서초3파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.485259, 127.016763],
+    radius=70,
+    popup='서초파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.484517, 127.037848],
+    radius=70,
+    popup='양재파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.471837, 127.027164],
+    radius=70,
+    popup='우면파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.561739, 127.036422],
+    radius=70,
+    popup='서울성동경찰서',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.554782, 127.019590],
+    radius=70,
+    popup='금호파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.493296, 127.076937],
+    radius=70,
+    popup='서울수서경찰서',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.479758, 127.051144],
+    radius=70,
+    popup='개포파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.465117, 127.107315],
+    radius=70,
+    popup='대왕파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.492535, 127.055959],
+    radius=70,
+    popup='대치지구대',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.484621, 127.046846],
+    radius=70,
+    popup='도곡지구대',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.489179, 127.104822],
+    radius=70,
+    popup='수서파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.492856, 127.082850],
+    radius=70,
+    popup='일원파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.516848, 126.865732],
+    radius=70,
+    popup='서울양천경찰서',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.535213, 126.878574],
+    radius=70,
+    popup='목1지구대',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.544549, 126.862347],
+    radius=70,
+    popup='목2지구대',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.532067, 126.832223],
+    radius=70,
+    popup='신월1파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.520002, 126.834856],
+    radius=70,
+    popup='신월2지구대',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.539107, 126.830662],
+    radius=70,
+    popup='신월5파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.520336, 126.860756],
+    radius=70,
+    popup='신정1지구대',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.510634, 126.871132],
+    radius=70,
+    popup='신정2지구대',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.514939, 126.854953],
+    radius=70,
+    popup='신정3파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.526121, 126.900809],
+    radius=70,
+    popup='서울영등포경찰서',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.533610, 126.901272],
+    radius=70,
+    popup='당산파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.498356, 126.898001],
+    radius=70,
+    popup='대림3파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.493449, 126.898680],
+    radius=70,
+    popup='대림파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.513601, 126.897659],
+    radius=70,
+    popup='문래지구대',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.513490, 126.914186],
+    radius=70,
+    popup='신길지구대',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.499110, 126.912753],
+    radius=70,
+    popup='신풍지구대',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.522692, 126.889661],
+    radius=70,
+    popup='양평파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.526944, 126.922323],
+    radius=70,
+    popup='여의도지구대',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.584158, 126.970562],
+    radius=70,
+    popup='청운파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.572879, 126.979125],
+    radius=70,
+    popup='청진파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.576773, 126.973972],
+    radius=70,
+    popup='통의파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.604825, 126.965235],
+    radius=70,
+    popup='평창파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.602042, 127.032206],
+    radius=70,
+    popup='서울종암경찰서',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.608962, 127.055233],
+    radius=70,
+    popup='석관파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.604444, 127.032380],
+    radius=70,
+    popup='월곡지구대',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.610019, 127.051386],
+    radius=70,
+    popup='장위지구대',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.600104, 127.031223],
+    radius=70,
+    popup='종암파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.618734, 127.104692],
+    radius=70,
+    popup='서울중랑경찰서',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.599083, 127.095551],
+    radius=70,
+    popup='망우지구대',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.615427, 127.082258],
+    radius=70,
+    popup='먹골파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.588198, 127.093845],
+    radius=70,
+    popup='면목본동파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.583212, 127.084118],
+    radius=70,
+    popup='면목삼팔파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.614787, 127.095491],
+    radius=70,
+    popup='봉화지구대',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.591330, 127.072813],
+    radius=70,
+    popup='상봉파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.580860, 127.079862],
+    radius=70,
+    popup='용마지구대',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.598013, 127.072191],
+    radius=70,
+    popup='중화지구대',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.563660, 126.989630],
+    radius=70,
+    popup='서울중부경찰서',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.565272, 127.013342],
+    radius=70,
+    popup='광희지구대',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.565048, 127.016349],
+    radius=70,
+    popup='신당파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.552148, 127.012348],
+    radius=70,
+    popup='약수지구대',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.566181, 126.992803],
+    radius=70,
+    popup='을지로3가파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.566480, 127.003908],
+    radius=70,
+    popup='을지지구대',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.558786, 127.004751],
+    radius=70,
+    popup='장충파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.484537, 126.996816],
+    radius=70,
+    popup='방배1파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.501225, 126.996781],
+    radius=70,
+    popup='서래파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.515513, 126.906661],
+    radius=70,
+    popup='영등포역파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.521505, 126.912983],
+    radius=70,
+    popup='중앙지구대',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.541168, 126.967511],
+    radius=70,
+    popup='서울용산경찰서',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.526799, 127.000131],
+    radius=70,
+    popup='보광파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.528896, 126.964023],
+    radius=70,
+    popup='용산역파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.549157, 126.978448],
+    radius=70,
+    popup='용중지구대',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.538594, 126.965405],
+    radius=70,
+    popup='원효지구대',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.520802, 126.971681],
+    radius=70,
+    popup='이촌파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.534314, 126.994368],
+    radius=70,
+    popup='이태원파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.531065, 127.006315],
+    radius=70,
+    popup='한남파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.628148, 126.928822],
+    radius=70,
+    popup='서울은평경찰서',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.611290, 126.929010],
+    radius=70,
+    popup='대조파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.621462, 126.926532],
+    radius=70,
+    popup='불광지구대',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.604519, 126.915110],
+    radius=70,
+    popup='역촌파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.615192, 126.912640],
+    radius=70,
+    popup='연신내지구대',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.636811, 126.918362],
+    radius=70,
+    popup='진관파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.575735, 126.984736],
+    radius=70,
+    popup='서울종로경찰서',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.569032, 126.988010],
+    radius=70,
+    popup='관수파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.572235, 126.961206],
+    radius=70,
+    popup='교남파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.576110, 126.967970],
+    radius=70,
+    popup='사직파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.581420, 126.981098],
+    radius=70,
+    popup='삼청파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.600067, 126.958428],
+    radius=70,
+    popup='세검정파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.568991, 126.976403],
+    radius=70,
+    popup='세종로파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.570861, 126.972262],
+    radius=70,
+    popup='신문로파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.581687, 126.969434],
+    radius=70,
+    popup='옥인파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.664572, 127.063583],
+    radius=70,
+    popup='마들지구대',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.652524, 127.077303],
+    radius=70,
+    popup='불암지구대',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.679704, 127.055093],
+    radius=70,
+    popup='상계1파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.633720, 127.059277],
+    radius=70,
+    popup='월계지구대',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.653778, 127.052803],
+    radius=70,
+    popup='서울도봉경찰서',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.679522, 127.043378],
+    radius=70,
+    popup='도봉1파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.684031, 127.049261],
+    radius=70,
+    popup='도봉2파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.664549, 127.040009],
+    radius=70,
+    popup='방학파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+folium.CircleMarker(
+    location=[37.647543, 127.025555],
+    radius=70,
+    popup='숭미파출소',
+    color='r',
+    fill=True,
+    fill_color='#ff1111'
+).add_to(map2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+map2.save('C:/Users/ezen/Desktop/map.html')
+
+
+
